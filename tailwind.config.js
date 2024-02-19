cff7eac52661905565fa7cd5779afdaac2da7407
David Holbrook <david@davidholbrook>
2024-02-19 14:29:28 -0600

    Added tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors:{
+      "primary": "#B6C649",
+      "secondary": "#C13953",
+      "dark": "#476A6F",
+      "liteblu": "#6CD4FF",
+      "lite": "#F1DEDE"
+    },
     extend: {},
   },
   plugins: [],
