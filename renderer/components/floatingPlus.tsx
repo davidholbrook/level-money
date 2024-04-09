ea21d7725f1f9a20811ee7b74420a690c7e16dac
David Holbrook <david@davidholbrook>
2024-04-09 14:52:08 -0500

    Added floating plus button

---
+++ renderer/components/floatingPlus.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const floatingPlus = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="#ffffff" className="w-14 h-14 floatingPlus cursor-pointer">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+    </svg>
+  )
+}
+
+export default floatingPlus;
--- renderer/layouts/layout.tsx
+++ renderer/layouts/layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../components/navagation";
+import FloatingPlus from "../components/floatingPlus";
 
 const Layout = ({ children }) => {
   return (
 	<>
 	  <Navbar />
 	  <main>{children}</main>
+	  <div><FloatingPlus /></div>
 	</>
   );
 };
