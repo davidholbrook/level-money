30421b1021f654430544c0d263d530a4c7241039
David Holbrook <david@davidholbrook>
2024-04-09 13:41:46 -0500

    Added react dev tools

--- main/background.js
+++ main/background.js
@@ -1,40 +1,42 @@
-import path from 'path'
-import { app, ipcMain } from 'electron'
-import serve from 'electron-serve'
-import { createWindow } from './helpers'
+import path from 'path';
+/* eslint-disable-next-line */
+import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
+/* eslint-disable-next-line */
+import { app } from 'electron';
+import serve from 'electron-serve';
+import { createWindow } from './helpers';
 
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production';
 
 if (isProd) {
-  serve({ directory: 'app' })
+  serve({ directory: 'app' });
 } else {
-  app.setPath('userData', `${app.getPath('userData')} (development)`)
+  app.setPath('userData', `${app.getPath('userData')} (development)`);
 }
 
-;(async () => {
-  await app.whenReady()
-
+(async () => {
+  await app.whenReady().then(() => {
+    installExtension(REACT_DEVELOPER_TOOLS)
+      .then((name) => console.log(`Added Extension:  ${name}`))
+      .catch((err) => console.log('An error occurred: ', err));
+  });
   const mainWindow = createWindow('main', {
     width: 1000,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
-  })
+  });
 
   if (isProd) {
-    await mainWindow.loadURL('app://./home')
+    await mainWindow.loadURL('app://./home');
   } else {
-    const port = process.argv[2]
-    await mainWindow.loadURL(`http://localhost:${port}/home`)
-    mainWindow.webContents.openDevTools()
+    const port = process.argv[2];
+    await mainWindow.loadURL(`http://localhost:${port}/home`);
+    mainWindow.webContents.openDevTools();
   }
-})()
+})();
 
 app.on('window-all-closed', () => {
-  app.quit()
-})
-
-ipcMain.on('message', async (event, arg) => {
-  event.reply('message', `${arg} World!`)
-})
+  app.quit();
+});
