1aa4920a0628ca5107cff77ba3cf7b8f83ba9ae8
David Holbrook <david@davidholbrook>
2024-04-09 21:39:19 -0500

    Add link to overview

--- renderer/components/navagation.tsx
+++ renderer/components/navagation.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 const Navagation = () => {
 	return (
-		<div className="flex justify-between bg-red-50">
-			<h1>Level Money</h1>
+		<div className="flex justify-between items-center bg-red-50 py-4 px-3">
+			<h1 className="text-2xl"><Link href="/home">Level Money</Link></h1>
 			<div className="flex gap-5">
 				<Link href="/monthly" className="text-black">Monthly</Link>
 				<Link href="/special">Special</Link>
